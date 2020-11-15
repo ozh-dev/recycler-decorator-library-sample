857308c2d329b2ec2bc1b8c5226e4938d9027323
Олег <zhilo@surfstudio.ru>
2020-11-15 15:42:23 +0300

    added gifs

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 
 ### Sample for [library recycler-decorator](https://gitlab.com/surfstudio/public/android-standard/-/tree/dev/G-0.5.0/recycler-decorator)
 
-![Sample](art/sample.gif)
+![Sample](art/complex_sample.gif)
--- app/src/main/java/ru/ozh/recycler/decorator/pager/CarouselDecoratorActivityView.kt
+++ app/src/main/java/ru/ozh/recycler/decorator/pager/CarouselDecoratorActivityView.kt
@@ -12,10 +12,12 @@ import ru.ozh.recycler.decorator.pager.controllers.Controller
 import ru.ozh.recycler.decorator.list.decor.RoundDecor
 import ru.ozh.recycler.decorator.list.decor.SimpleOffsetDrawer
 import kotlinx.android.synthetic.main.activity_pager.*
+import ru.ozh.recycler.decorator.pager.controllers.IndicatorAlign
 import ru.surfstudio.android.easyadapter.EasyAdapter
 import ru.surfstudio.android.easyadapter.ItemList
 import ru.surfstudio.android.recycler.decorator.Decorator
 import ru.ozh.recycler.decorator.pager.controllers.LinePagerIndicatorDecoration
+import ru.ozh.recycler.decorator.pager.controllers.ScaleLinePageIndicatorDecoration
 import ru.ozh.recycler.decorator.px
 
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
@@ -43,12 +45,14 @@ class CarouselDecoratorActivityView : AppCompatActivity() {
 
         val simpleOffsetDrawer2 = SimpleOffsetDrawer(
             left = 16.px,
-            right = 16.px
+            right = 16.px,
+            bottom = 32.px
         )
 
         val decorator2 = Decorator.Builder()
             .underlay(roundViewHoldersGroupDrawer)
-            .overlay(LinePagerIndicatorDecoration())
+//            .overlay(LinePagerIndicatorDecoration())
+            .overlay(ScaleLinePageIndicatorDecoration(context = this))
             .offset(simpleOffsetDrawer2)
             .build()
 
